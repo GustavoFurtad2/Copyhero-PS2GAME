3347b7e346aa2483fbb7fa18cebacd0fc5fb171a
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-02-07 19:44:51 -0300

    Update image.js

--- image.js
+++ image.js
@@ -1,19 +1,24 @@
 export let images = {
-    "circle"  : new Image("assets/circle.png"),
-    "cross"   : new Image("assets/cross.png"),
-    "down"    : new Image("assets/down.png"),
-    "L1"      : new Image("assets/L1.png"),
-    "L2"      : new Image("assets/L2.png"),
-    "L3"      : new Image("assets/L3.png"),
-    "left"    : new Image("assets/left.png"),
-    "R1"      : new Image("assets/R1.png"),
-    "R2"      : new Image("assets/R2.png"),
-    "R3"      : new Image("assets/R3.png"),
-    "right"   : new Image("assets/right.png"),
-    "select"  : new Image("assets/select.png"),
-    "square"  : new Image("assets/square.png"),
-    "start"   : new Image("assets/start.png"),
-    "triangle": new Image("assets/triangle.png"),
-    "up"      : new Image("assets/up.png"),
-    "logo"    : new Image("assets/logo.png")
+    "circle"           : new Image("assets/circle.png"),
+    "cross"            : new Image("assets/cross.png"),
+    "down"             : new Image("assets/down.png"),
+    "L1"               : new Image("assets/L1.png"),
+    "L2"               : new Image("assets/L2.png"),
+    "L3"               : new Image("assets/L3.png"),
+    "left"             : new Image("assets/left.png"),
+    "R1"               : new Image("assets/R1.png"),
+    "R2"               : new Image("assets/R2.png"),
+    "R3"               : new Image("assets/R3.png"),
+    "right"            : new Image("assets/right.png"),
+    "select"           : new Image("assets/select.png"),
+    "square"           : new Image("assets/square.png"),
+    "start"            : new Image("assets/start.png"),
+    "triangle"         : new Image("assets/triangle.png"),
+    "up"               : new Image("assets/up.png"),
+    "logo"             : new Image("assets/logo.png"),
+    "background_red"   : new Image("assets/background_red.png"),
+    "background_white" : new Image("assets/background_white.png")
 }
+
+images.background_white.width = 640
+images.background_white.height = 448
