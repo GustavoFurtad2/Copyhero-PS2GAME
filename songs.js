6ea13c5bd902feeff7798d6717d71db1d925f935
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-02-20 20:46:47 -0300

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,18 @@
+const SONGS = {
+    "1" : {
+        "name"  : "Balaclava",
+        "author": "Arctic Monkeys",
+        "album" : "Favourite Worst Nightmare",
+    },
+    "2" : {
+        "name"  : "Old Yellow Bricks",
+        "author": "Arctic Monkeys",
+        "album" : "Favourite Worst Nightmare",
+    },
+    "3" : {
+        "name"  : "Pretty Visitors",
+        "author": "Arctic Monkeys",
+        "album" : "Humbug",
+    },
+
+}
